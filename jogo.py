6d167295acfebdc509fc9e65f28defce0ff88519
Lucas Silva <lucas030894@gmail.com>
2022-10-13 18:17:09 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -6,19 +6,21 @@ md.titulo('Bem vindo ao jogo de advinhação!')
 
 numero_secreto = randint(0,100)
 
-tentativa = 1
 max_tentativas = 3
 
-while tentativa <= max_tentativas:
+for tentativa in range(1, max_tentativas + 1):
     print(f'Tentativa {tentativa} de {max_tentativas}')
     chute = int(input('Digite um número de 1 a 100: '))
-    if chute == numero_secreto:
-        print('Você acertou')
-        break
-    elif chute > numero_secreto:
-        print('Seu chute foi maior que o número secreto')
-    elif chute < numero_secreto:
-        print('Seu chute foi menor que o número secreto')
-    tentativa += 1
+    if 0 <= chute <= 100:
+        if chute == numero_secreto:
+            print('Você acertou')
+            break
+        elif chute > numero_secreto:
+            print('Seu chute foi maior que o número secreto', numero_secreto)
+        elif chute < numero_secreto:
+            print('Seu chute foi menor que o número secreto')
+    else:
+        print('Você digitou um número invalido')
+        tentativa -= 1
 
 print('Fim do Jogo')
