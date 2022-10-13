095105e0bafc3e6596e3f1f4ee316835cc56b73a
Lucas Silva <lucas030894@gmail.com>
2022-10-13 19:47:52 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -2,12 +2,30 @@
 import mods as md
 from random import randint
 
+# Inicio do programa onde é mostrado o titulo do programa e os niveis de dificuldade
 md.titulo('Bem vindo ao jogo de advinhação!')
 
-numero_secreto = randint(0,100)
+numero_secreto = randint(0,100) # geração aleátoria de um número de 0 a 100
 
-max_tentativas = 3
+print('Escolha seu nivel de dificuldade')
+print('(1) Facil (2) Médio (3) Dificil')
 
+# Escolha do nivel de dificuldade pelo usuário. obs: enquanto o usuário não escolher entre os niveis 1, 2 e 3 ele ficará preso em um loop infinito
+while True:
+    nivel = input('Qual nivel de dificuldade você quer jogar: ')
+    if nivel == '1':
+        max_tentativas = 20
+        break
+    elif nivel == '2':
+        max_tentativas = 10
+        break
+    elif nivel == '3':
+        max_tentativas = 5
+        break
+    else:
+        print('Este nivel de dificuldade não existe.')
+
+# Comparação do valor gerado pelo computador com o valor que será escolhido pelo usuário. obs: o número de tentativas é definido pelo nivel de dificuldade escolhido acima.
 for tentativa in range(1, max_tentativas + 1):
     print(f'Tentativa {tentativa} de {max_tentativas}')
     chute = int(input('Digite um número de 1 a 100: '))
@@ -16,11 +34,12 @@ for tentativa in range(1, max_tentativas + 1):
             print('Você acertou')
             break
         elif chute > numero_secreto:
-            print('Seu chute foi maior que o número secreto', numero_secreto)
+            print('Seu chute foi maior que o número secreto')
         elif chute < numero_secreto:
             print('Seu chute foi menor que o número secreto')
     else:
         print('Você digitou um número invalido')
         tentativa -= 1
 
+# Fim do jogo
 print('Fim do Jogo')
