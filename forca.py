21c7f4a2daae60a7f29045eba69d79925a0b713b
Lucas Silva <lucas030894@gmail.com>
2022-10-16 18:57:10 -0300

    Update forca.py

--- forca.py
+++ forca.py
@@ -14,18 +14,28 @@ def jogar():
     # Condições iniciais do jogo
     enforcou = False
     acertou = False
+    erros = 0
 
+    # Laço para fazer a analise das palavras chutadas
     while not enforcou and not acertou:
         print(letras_acertadas)
         chute = input('Digite uma letra: ').strip().upper()
         posicao = 1
-        for letra in palavra_secreta:
-            if letra == chute:
-                letras_acertadas[posicao-1] = chute
-            posicao += 1
+        if chute in palavra_secreta:
+            for letra in palavra_secreta:
+                if letra == chute:
+                    letras_acertadas[posicao-1] = chute
+                posicao += 1
+        else:
+            erros += 1
+            print(f'Você ainda tem {6-erros} tentativas antes de ser enforcado')
         if '_' not in letras_acertadas:
+            print(letras_acertadas)
             print('Parabens, Você conseguiu descobrir a palavra')
-            break
+            acertou = True
+        if erros == 6:
+            print('Você foi enforcado')
+            enforcou = True
 
     print('Fim do jogo')
 
