73d0a49924e04e71c92f4c0d97661449aba6eed1
Lucas Silva <lucas030894@gmail.com>
2022-10-18 15:59:43 -0300

    update final

--- Jogos.py
+++ Jogos.py
@@ -2,11 +2,13 @@ import mods as md
 import advinhacao
 import forca
 
-md.titulo('Escolha seu jogo')
-print('(1) Advinhação (2) Forca')
-jogo = input('Qual jogo você quer jogar: ')
-
-if jogo == '1':
-    advinhacao.jogar()
-elif jogo == '2':
-    forca.jogar()
+while True:
+    md.titulo('Escolha seu jogo')
+    print('(1) Advinhação (2) Forca')
+    jogo = input('Qual jogo você quer jogar: ')
+    if jogo == '1':
+        advinhacao.jogar()
+    elif jogo == '2':
+        forca.jogar()
+    else:
+        break
